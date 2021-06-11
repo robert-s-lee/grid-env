0f7c7e55b0acbf5c3dfa5c5640ca50820983298e
Robert S Lee <sangkyulee@gmail.com>
2021-06-11 12:28:05 -0400

    add req for K8s

--- argecho.py
+++ argecho.py
@@ -1,7 +1,20 @@
 import sys
 import os
 from kubernetes import client, config
+import pkg_resources
 
+# a simple script to echo arguments and dump out K8s environment
+# https://github.com/kubernetes-client/python
+
+# args
 print(f'Number of arguments: {len(sys.argv)} arguments.')
 print(f'Argument List: {str(sys.argv)}')
+
+# current wd
 print(f'os.getcwd={os.getcwd()}')
+
+# python packages
+installed_packages = pkg_resources.working_set
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+   for i in installed_packages])
+print(installed_packages_list)
