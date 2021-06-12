1019d10923bca90938aed4940b4262a8c60ac6e9
robert-s-lee <sangkyulee@gmail.com>
2021-06-12 13:05:39 -0400

    move k8s to different example

--- argecho.py
+++ argecho.py
@@ -1,19 +1,32 @@
 import sys
 import os
-import kubernetes.client
 import pkg_resources
-
-# a simple script to echo arguments and dump out K8s environment
-# https://github.com/kubernetes-client/python
+import subprocess
 
 # args
+print("Arguments:")
 print(f'Number of arguments: {len(sys.argv)} arguments.')
 print(f'Argument List: {str(sys.argv)}')
 
 # current wd
+print("\nWorking Directory:")
 print(f'os.getcwd={os.getcwd()}')
 
+# what are mounted
+print("\nFile Systems Mounted:")
+result = subprocess.Popen(['df', '-kH'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+while(True):
+   retcode = result.poll() 
+   line = result.stdout.readline()
+   print(line.decode("utf-8").strip())
+   if retcode is not None:
+      break
+
 # python packages
+print("\nPython Version:")
+print(sys.version)
+
+print("\nPython Packages:")
 installed_packages = pkg_resources.working_set
 installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
    for i in installed_packages])
